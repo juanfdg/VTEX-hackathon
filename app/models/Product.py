1181536b38fd794526b9933b4fdc9d75a11fee6d
Heladio Lopes <heladio.lopes@gmail.com>
2019-03-23 17:58:35 -0300

    Database

--- app/models/Product.py
+++ app/models/Product.py
@@ -1,11 +1,21 @@
-class Product:
-    def __init__(self, id, thumb, name, price, description):
-        self.id = id
-        self.thumb = thumb
-        self.name = name
-        self.price = price
-        self.description = description
+from app import db
 
-    def add_customization(self):
-        pass
 
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    thumb = db.Column(db.String(50), index=True, unique=True)
+    name = db.Column(db.String(30), index=True, unique=True)
+    price = db.Column(db.Float, index=True, unique=True)
+    description = db.Column(db.Text(300), index=True, unique=True)
+
+    # def __init__(self, id, thumb, name, price, description):
+    #     self.id = id
+    #     self.thumb = thumb
+    #     self.name = name
+    #     self.price = price
+    #     self.description = description
+
+    def __repr__(self):
+        return '<Name = {}>'.format(self.name)
+
+# p = Product(thumb="dummy.jpeg", name="Caixa", price=4.90, description="Caixa para embalar presente")
