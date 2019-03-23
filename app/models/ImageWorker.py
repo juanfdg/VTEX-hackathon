e413cea5c314e44b0155ab8b68c0c159b6e62c4f
name <juanfdantas@gmail.com>
2019-03-23 18:22:15 -0300

    Centering text in box x and y

--- app/models/ImageWorker.py
+++ app/models/ImageWorker.py
@@ -3,7 +3,7 @@ import os
 from Product import Product, Font
 
 
-def customize(product, texts):
+def customize(product, text):
     # Create image with input image
     image = Image.open(product.thumb)
 
@@ -11,19 +11,21 @@ def customize(product, texts):
     # object as background
     draw = ImageDraw.Draw(image)
 
-    for idx, custom in enumerate(product.customizations):
-        # Create font object with the font file and specify
-        # desired size
-        font = ImageFont.truetype(custom.font.font, custom.font.size)
+    # Create font object with the font file and specify
+    # desired size
+    font = ImageFont.truetype(product.font_path, product.font_size)
 
-        # starting position of message
-        (x, y) = (custom.box.x, custom.box.y)
+    # starting position of message
+    (x, y) = (product.box_x, product.box_y)
 
-        # Draw message on the background
-        draw.text((x, y), texts[idx], fill=custom.font.color, font=font)
+    w, h = draw.textsize(text)
+
+    # Draw message on the background
+    draw.text(((x-w)/2, (y-h)/2), text, fill=product.font.color, font=font)
 
     # Return the edited image
     filename, file_extension = os.path.splitext(product.thumb)
+    filename = os.path.relpath(filename, '../static')
 
     custom_file = '../temporary/' + filename + "_custom" + file_extension
     image.save(custom_file)
@@ -37,6 +39,6 @@ if __name__ == '__main__':
 
     product = Product(0, file, 'dummy', 0.0, 'a_dummy_product')
     font = Font(font, 20, (0, 200, 100))
-    product.add_customization('Name', font, 80, 50, 20, 20)
+    product.add_customization('Name', font, 100, 50, 20, 20)
 
     print(customize(product, ['Dummy']))
