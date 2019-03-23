2d8da4eb6b141cbeaafa93217abfafedd6cc8bf9
name <juanfdantas@gmail.com>
2019-03-23 17:07:38 -0300

    effectively saving a custom figure, but in static folder

--- app/models/Product.py
+++ app/models/Product.py
@@ -1,3 +1,25 @@
+class Box:
+    def __init__(self, x, y, length, height):
+        self.x = x
+        self.y = y
+        self.length = length
+        self.height = height
+
+
+class Font:
+    def __init__(self, font, font_size, font_color):
+        self.font = font
+        self.size = font_size
+        self.color = font_color
+
+
+class Customization:
+    def __init__(self, name, font, x, y, l, h):
+        self.name = name
+        self.font = font
+        self.box = Box(x, y, l, h)
+
+
 class Product:
     def __init__(self, id, thumb, name, price, description):
         self.id = id
@@ -5,7 +27,8 @@ class Product:
         self.name = name
         self.price = price
         self.description = description
+        self.customizations = []
 
-    def add_customization(self):
-        pass
+    def add_customization(self, name, font, x, y, l, h):
+        self.customizations.append(Customization(name, font, x, y, l, h))
 
