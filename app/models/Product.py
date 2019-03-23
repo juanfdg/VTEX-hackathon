262cbee531014f2a43118f92f12c4d7efed97762
Heladio Lopes <heladio.lopes@gmail.com>
2019-03-23 15:52:06 -0300

    Product Class

---
+++ app/models/Product.py
@@ -0,0 +1,11 @@
+class Product:
+    def __init__(self, id, thumb, name, price, description):
+        self.id = id
+        self.thumb = thumb
+        self.name = name
+        self.price = price
+        self.description = description
+
+    def add_customization(self):
+        pass
+
